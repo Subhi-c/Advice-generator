bfb14e5137dbd889646f0496fe840d24017872a4
ChugilC <chugilrc13@gmail.com>
2023-04-29 09:37:22 +0530

    Remove console log

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const qoutesEl = document.getElementById("qoutes");
-const btnEl = document.getElementById("qoute-btn");
-const idNumber = document.getElementById("idNo");
+const qoutesEl = document.getElementById('qoutes');
+const btnEl = document.getElementById('qoute-btn');
+const idNumber = document.getElementById('idNo');
 //let qoute = "";
 const generateqoute = async () => {
-  const result = await fetch("https://api.adviceslip.com/advice");
-    const data = await result.json();
-    console.log(data)
-    qoutesEl.innerText = data.slip.advice;
-    idNumber.innerText = data.slip.id
+  const result = await fetch('https://api.adviceslip.com/advice');
+  const data = await result.json();
+
+  qoutesEl.innerText = data.slip.advice;
+  idNumber.innerText = data.slip.id;
 };
 // function generateqoute() {
 //   fetch("https://api.adviceslip.com/advice")
